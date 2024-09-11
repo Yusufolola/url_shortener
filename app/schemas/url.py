8e54f631a80e47d77567c549f0d0bea981e4cc1e
OYESANMI Yusuf <134579084+Yusufolola@users.noreply.github.com>
2024-09-11 13:22:42 +0100

    5th Update url.py

--- app/schemas/url.py
+++ app/schemas/url.py
@@ -1,11 +1,19 @@
 from pydantic import BaseModel, HttpUrl, Field
 from typing import Optional
 
-class URLCreate(BaseModel):
+class URLBase(BaseModel):
     original_url: HttpUrl
+
+class URLCreate(URLBase):
     length: Optional[int] = Field(6, ge=4, le=12, description="Length of the shortened URL")
-    
+
 class URLUpdate(BaseModel):
     original_url: Optional[HttpUrl] = None
     length: Optional[int] = Field(6, ge=4, le=12, description="New length of the shortened URL")
 
+class URL(URLBase):
+    id: int
+    shortened_url: Optional[str]
+
+    class Config:
+        orm_mode = True
