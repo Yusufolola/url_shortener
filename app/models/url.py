4bc13b9e8e8ba5655571efdde13eb94ce0fab181
OYESANMI Yusuf <134579084+Yusufolola@users.noreply.github.com>
2024-09-11 13:26:49 +0100

    6th Update url.py

--- app/models/url.py
+++ app/models/url.py
@@ -1,18 +1,9 @@
-from pydantic import BaseModel, AnyHttpUrl
-from typing import Optional
-class URLBase(BaseModel):
-    original_url: AnyHttpUrl
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
 
-class URLCreate(URLBase):
-    length: int
+class URLModel(Base):
+    __tablename__ = "urls"
 
-class URLUpdate(BaseModel):
-    original_url: AnyHttpUrl = None
-    length: int = None
-
-class URL(URLBase):
-    id: int
-    shortened_url: Optional [str]
-
-    class Config:
-        orm_mode = True
+    id = Column(Integer, primary_key=True, index=True)
+    original_url = Column(String, nullable=False)
+    shortened_url = Column(String, nullable=False)
