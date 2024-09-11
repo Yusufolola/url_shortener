14ea069977b13371e2317033212d9e8c9039b600
OYESANMI Yusuf <134579084+Yusufolola@users.noreply.github.com>
2024-09-11 20:14:27 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from app.api import routes
 from app.configuration.config import settings
 from fastapi.staticfiles import StaticFiles
+from app.db.session import init_db
 
 
 app = FastAPI(title="URL Shortener")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(routes.router)
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 if __name__ == "__main__":
     import uvicorn
