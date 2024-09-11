88a8287e670c235fc13840dc6e65ce20f60427b6
OYESANMI Yusuf <134579084+Yusufolola@users.noreply.github.com>
2024-09-11 10:01:38 +0100

    Update config.py

--- app/configuration/config.py
+++ app/configuration/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./url.db"
+    DATABASE_URL: str = postgresql://url_user:qBvV7npSVZnZvSrMjmbz2BOcv3T1H5LE@dpg-crgj6jjv2p9s73achkt0-a/url_v7th
     PORT: int = 8000
 
 settings = Settings()
