7d2fcc73224944392bc5f309575d0afa6a6e4017
OYESANMI Yusuf <134579084+Yusufolola@users.noreply.github.com>
2024-09-12 05:42:38 +0100

    21th Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app.db.session import get_db
@@ -23,6 +23,18 @@ def shorten_url(url_create: URLCreate, db: Session = Depends(get_db)):
     short_url = crud.create_url(db, url_create)
     full_shortened_url = f"https://url-shortener-4-2owt.onrender.com/{short_url}"
     return {"shortened_url": full_shortened_url}
+
+@router.get("/{shortened_path}")
+def redirect_to_url(shortened_path: str, db: Session = Depends(get_db)):
+    # Look up the original URL associated with the shortened path
+    db_url = db.query(URLModel).filter(URLModel.shortened_url == shortened_path).first()
+    
+    if db_url:
+        # Redirect to the original URL
+        return RedirectResponse(url=db_url.original_url)
+    else:
+        # If the shortened URL does not exist, raise a 404 error
+        raise HTTPException(status_code=404, detail="Shortened URL not found")
 
 @router.put("/{short_url}")
 def update_url(short_url: str, url_update: URLUpdate, db: Session = Depends(get_db)):
